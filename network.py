7a71a79f469cfa8c89e22f9e1723c06fcd1a4978
Oscar Eduardo González Jaramillo <oscareduardogj2004@gmail.com>
2025-09-04 23:58:22 -0600

    Corregí muchos errores acerca del optimizador ADAM mejoro mucho

--- network.py
+++ network.py
@@ -42,6 +42,11 @@ class Network(object):
 
         self.weights = [np.random.normal(0, 1/np.sqrt(x), (y, x))  
                         for x, y in zip(sizes[:-1], sizes[1:])]
+        self.m_b = [np.zeros(b.shape) for b in self.biases]
+        self.m_w = [np.zeros(w.shape) for w in self.weights]
+        self.r_b = [np.zeros(b.shape) for b in self.biases]
+        self.r_w = [np.zeros(w.shape) for w in self.weights]
+
     """
     Definimos la función de feedforward, la cual nos permitirá entrenar y posteriormente
     utilizar la red en el momento que necesitemos. Esta función espera un vector de entrada
@@ -55,7 +60,7 @@ class Network(object):
             a = sigmoid(np.dot(w, a)+b)
         return a
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta, beta_1, beta_2, test_data=None):
+    def ADAM(self, training_data, epochs, mini_batch_size, eta, beta_1, beta_2, test_data=None):
         """
         En esta parte se entrena la red neuronal al definir un conjunto de datos de entrenamiento, 
         los datos de prueba, las épocas, la tasa de aprendizaje y el tamaño del mini batch. 
@@ -80,8 +85,12 @@ class Network(object):
         # En esta sección se empaquetan las imágenes en mini batches de manera aleatoria para evitar 
         # que la red aprenda patrones dados por el orden de las imágenes y permitir una mejor aproximación 
         # de la función de costos "real".
+                #Inicializamos los parámetros usados en el optimizador adam del mismo 
+        #tamaño que los pesos y bias 
+
         t=0
         for j in range(epochs):
+
             random.shuffle(training_data)
             mini_batches = [training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
@@ -112,33 +121,33 @@ class Network(object):
         # biases y pesos para poder actualizarlos correctamente 
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        m_b = [np.zeros(b.shape) for b in self.biases]
-        m_w = [np.zeros(w.shape) for w in self.weights]
-        r_b = [np.zeros(b.shape) for b in self.biases]
-        r_w = [np.zeros(w.shape) for w in self.weights]
+
         # Calculamos y almacenamos los valores de las derivadas parciales de cada 
         # peso y bias; en otras palabras, calculamos la "culpa" de cada peso y bias. 
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        #Definimos los parámetros del optimizador adam para el peso 
-        m_w = [beta_1 * y + (1-beta_1) * x for x, y in zip(nabla_w, m_w)]
-        r_w = [beta_2 * y + (1-beta_2) * x**2 for x, y in zip(nabla_w, r_w)]
-        m_hat_w = [x/(1-beta_1**t) for x in m_w]
-        r_hat_w = [x/(1-beta_2**t) for x in r_w]
-        #Definimos los parámetros del optimizador adam para los bias 
-        m_b = [beta_1 * y + (1-beta_1) * x for x, y in zip(nabla_b, m_b)]
-        r_b = [beta_2 * y + (1-beta_2) * x**2 for x, y in zip(nabla_b, r_b)]
-        m_hat_b = [x/(1-beta_1**t) for x in m_b]
-        r_hat_b = [x/(1-beta_2**t) for x in r_b]
-
         nabla_w = [x/len(mini_batch) for x in nabla_w]
         nabla_b = [x/len(mini_batch) for x in nabla_b]
-        self.weights = [w-((eta*mhw)/(np.sqrt(rhw) + 0.000000000001))*nw
-                        for w, nw, mhw, rhw in zip(self.weights, nabla_w, m_hat_w, r_hat_w)]
-        self.biases = [b-((eta*mhb)/(np.sqrt(rhb) + 0.000000000001))*nb
-                       for b, nb, mhb, rhb in zip(self.biases, nabla_b, m_hat_b, r_hat_b)]
+        #Definimos los parámetros del optimizador adam para el peso 
+        self.m_w = [beta_1 * y + (1-beta_1) * x for x, y in zip(nabla_w, self.m_w)]
+        self.r_w = [beta_2 * y + (1-beta_2) * x**2 for x, y in zip(nabla_w, self.r_w)]
+        #Corregimos el sezgo inicial
+        m_hat_w = [x/(1-beta_1**t) for x in self.m_w]
+        r_hat_w = [x/(1-beta_2**t) for x in self.r_w]
+        #Definimos los parámetros del optimizador adam para los bias 
+        self.m_b = [beta_1 * y + (1-beta_1) * x for x, y in zip(nabla_b, self.m_b)]
+        self.r_b = [beta_2 * y + (1-beta_2) * x**2 for x, y in zip(nabla_b, self.r_b)]
+        #Corregimos el sezgo inicial
+        m_hat_b = [x/(1-beta_1**t) for x in self.m_b]
+        r_hat_b = [x/(1-beta_2**t) for x in self.r_b]
+
+        #Actualizamos los pesos y bias 
+        self.weights = [w-((eta*mhw)/(np.sqrt(rhw) + 10e-8))
+                        for w, mhw, rhw in zip(self.weights, m_hat_w, r_hat_w)]
+        self.biases = [b-((eta*mhb)/(np.sqrt(rhb) + 10e-8))
+                        for b, mhb, rhb in zip(self.biases, m_hat_b, r_hat_b)]
 
     def backprop(self, x, y):
         """
